5d51c2176d4465ced02ddc3160a942d9beb9c1cc
nitesh-gupta-64 <123109031@nitkkr.ac.in>
2024-05-27 18:09:57 +0530

    checking

--- src/Components/HeadTitle/HeadTitle.jsx
+++ src/Components/HeadTitle/HeadTitle.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HeadTitle = ({headTitle}) => {
   return (
     <div>
-        <h2 style={{margin: '2rem 5rem 0.5rem 5rem', fontSize: '2rem', fontWeight:'400'}}>{headTitle}</h2>
+        <h2 style={{margin: '2rem 0rem 0.5rem 5rem', fontSize: '2rem', fontWeight:'400'}}>{headTitle}</h2>
     </div>
   )
 }
